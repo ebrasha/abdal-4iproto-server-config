d5ed33940cb5c03157837bf523db7ca3ba0bba33
abdal-security-group <abdal.group@gmail.comgit>
2023-05-04 19:20:52 +0330

    ver 2.8

--- debian.11-x64-installer.sh
+++ debian.11-x64-installer.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 
 apt update
 apt install wget curl tar zip unzip -y
 cd /usr/local/
-url_zip_file="https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/abdal-4iproto-server-config.zip"
+url_zip_file="https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/abdal-4iproto-server-config-debian.11-x64.zip"
 wget -N --no-check-certificate -O /usr/local/abdal-4iproto-server-config.zip ${url_zip_file}
 unzip /usr/local/abdal-4iproto-server-config.zip
 cd /usr/local/abdal-4iproto-server-config
 chmod +x Abdal4iProtoServerConfig
 rm -f /usr/local/bin/Abdal4iProtoServerConfig
 ln -s /usr/local/abdal-4iproto-server-config/Abdal4iProtoServerConfig /usr/local/bin
-
+mkdir /usr/local/abdal-4iproto-server-config-db/
 echo Abdal 4iProto Server Config installed successfully....
 rm -f /usr/local/abdal-4iproto-server-config.zip
+
+echo " "
+echo -e "${red}---------------------------------${plain}"
+echo -e "${green}Abdal 4iProto Server Config installed successfully....${plain}"
+echo -e "${green}If need more options, let us know: Prof.Shafiei@Gmail.com${plain}"
+echo -e "${red}---------------------------------${plain}"
+
+
--- debian.11-x64-uninstaller.sh
+++ debian.11-x64-uninstaller.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
 
 rm -rf /usr/local/abdal-4iproto-server-config
 rm -f /usr/local/bin/Abdal4iProtoServerConfig
 rm -f /usr/local/abdal-4iproto-server-config.zip
 clear
-echo Abdal 4iProto Server Config uninstalled successfully....
+
+
+echo " "
+echo -e "${red}---------------------------------${plain}"
+echo -e "${green}Abdal 4iProto Server Config uninstalled successfully....${plain}"
+echo -e "${yellow}If there is a problem with the program, let us know: Prof.Shafiei@Gmail.com${plain}"
+echo -e "${red}---------------------------------${plain}"
+
