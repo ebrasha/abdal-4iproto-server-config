a138cc0221872021654e0bf0ba25c11244e61ad9
abdal-security-group <abdal.group@gmail.comgit>
2023-05-04 05:42:55 +0330

    Update debian.11-x64.sh

--- debian.11-x64.sh
+++ debian.11-x64.sh
@@ -2,14 +2,14 @@
 
 apt update
 apt install wget curl tar zip unzip -y
-cd /usr/local
+cd /usr/local/
 url_zip_file="https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/abdal-4iproto-server-config.zip"
-wget -N --no-check-certificate -O /usr/local/abdal-4iproto-server-config.zip  ${url_zip_file}
+wget -N --no-check-certificate -O /usr/local/abdal-4iproto-server-config.zip ${url_zip_file}
 unzip abdal-4iproto-server-config.zip
 rm -f abdal-4iproto-server-config.zip
-cd abdal-4iproto-server-config
-chmod +x  Abdal4iProtoServerConfig
-rm -f  /usr/local/bin/Abdal4iProtoServerConfig
-ln -s /usr/local/abdal-4iproto-server-config/Abdal4iProtoServerConfig   /usr/local/bin
+cd /usr/local/abdal-4iproto-server-config
+chmod +x Abdal4iProtoServerConfig
+rm -f /usr/local/bin/Abdal4iProtoServerConfig
+ln -s /usr/local/abdal-4iproto-server-config/Abdal4iProtoServerConfig /usr/local/bin
 
 echo Abdal 4iProto Server Config installed successfully....
