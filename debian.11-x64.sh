3c8b78ac0acd5cc423ec7e0a0be109182430ebf9
abdal-security-group <abdal.group@gmail.comgit>
2023-05-04 05:45:41 +0330

    Update debian.11-x64.sh

--- debian.11-x64.sh
+++ debian.11-x64.sh
@@ -6,7 +6,7 @@ cd /usr/local/
 url_zip_file="https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/abdal-4iproto-server-config.zip"
 wget -N --no-check-certificate -O /usr/local/abdal-4iproto-server-config.zip ${url_zip_file}
 unzip /usr/local/abdal-4iproto-server-config.zip
-rm -f /usr/local/abdal-4iproto-server-config.zip
+ 
 cd /usr/local/abdal-4iproto-server-config
 chmod +x Abdal4iProtoServerConfig
 rm -f /usr/local/bin/Abdal4iProtoServerConfig
