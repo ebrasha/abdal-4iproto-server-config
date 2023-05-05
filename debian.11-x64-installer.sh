329adc3fb4559357fd1a668139b669552dcfe136
abdal-security-group <abdal.group@gmail.comgit>
2023-05-05 05:36:42 +0330

    Update debian.11-x64-installer.sh

--- debian.11-x64-installer.sh
+++ debian.11-x64-installer.sh
@@ -5,10 +5,48 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 installer() {
+  ## variables
+  os_command=""
+  url_zip_file=""
+
+  echo -e "${green}Please select your operating system version:${plain}"
+  echo -e "${yellow}1. debian.11-x64${plain}"
+  echo -e "${yellow}2. debian.10-x64${plain}"
+  echo -e "${yellow}3. ubuntu.22.04-x64${plain}"
+  echo -e "${yellow}4. ubuntu.20.04-x64${plain}"
+  echo -e "${yellow}5. ubuntu.18.04-x64${plain}"
+  echo -e "${yellow}6. centos.7-x64${plain}"
+  echo -e "${yellow}7. rhel.7-x64${plain}"
+  echo -e "${yellow}8. rhel.8-x64${plain}"
+  echo -e "${yellow}9. fedora.37-x64${plain}"
+
+  read os_selector
+
+
+  case $os_selector in
+
+    1)
+      echo -n "Debian"
+      exit 1
+      ;;
+
+    Romania | Moldova)
+      echo -n "Romanian"
+      ;;
+
+    Italy | "San Marino" | Switzerland | "Vatican City")
+      echo -n "Italian"
+      ;;
+
+    *)
+      echo -n "unknown"
+      ;;
+  esac
+
+
   apt update
   apt install wget curl tar zip unzip -y
   cd /usr/local/
-  url_zip_file="https://raw.githubusercontent.com/ebrasha/abdal-4iproto-server-config/main/abdal-4iproto-server-config-debian.11-x64.zip"
   wget -N --no-check-certificate -O /usr/local/abdal-4iproto-server-config.zip ${url_zip_file}
   unzip /usr/local/abdal-4iproto-server-config.zip
   cd /usr/local/abdal-4iproto-server-config
@@ -16,7 +54,6 @@ installer() {
   rm -f /usr/local/bin/Abdal4iProtoServerConfig
   ln -s /usr/local/abdal-4iproto-server-config/Abdal4iProtoServerConfig /usr/local/bin
   mkdir /usr/local/abdal-4iproto-server-config-db/
-  echo Abdal 4iProto Server Config installed successfully....
   rm -f /usr/local/abdal-4iproto-server-config.zip
 
   echo " "
@@ -29,5 +66,8 @@ installer() {
 
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}mistake：${plain} This script must be run with the root user！\n" && exit 1
+
+
+
 
 installer
